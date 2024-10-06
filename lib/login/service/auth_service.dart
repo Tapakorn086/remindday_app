41923a2a349ac275656ee0d4e83a993881f5e76c
Tapakorn086 <nakason_t@silpakorn.edu>
2024-10-06 18:49:34 +0700

    update: serivce for login

--- lib/login/service/auth_service.dart
+++ lib/login/service/auth_service.dart
@@ -1,24 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart'; 
+import 'package:http/http.dart' as http; 
+import 'dart:convert'; 
+
 class AuthService {
-  bool _isLoggedIn = false;
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  final String _loginUrl = 'http://192.168.1.105:8080/api/login'; 
 
   Future<bool> isLoggedIn() async {
-    // ในการใช้งานจริง คุณอาจจะต้องตรวจสอบ token ที่เก็บไว้ใน secure storage
-    return _isLoggedIn;
+    // ตรวจสอบ token ใน secure storage
+    String? token = await _storage.read(key: 'token');
+    return token != null;
   }
 
   Future<bool> login(String username, String password) async {
-    // Simulate API call
-    await Future.delayed(Duration(seconds: 2));
-    if (username == 'user' && password == 'password') {
-      _isLoggedIn = true;
-      return true;
+    try {
+      final response = await http.post(
+        Uri.parse(_loginUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'email': username, 'password': password}),
+      );
+
+      if (response.statusCode == 200) {
+        // สมมุติว่าคุณได้รับ token จาก API
+        final data = jsonDecode(response.body);
+        String token = data['token']; 
+        await _storage.write(key: 'token', value: token);
+        return true;
+      }
+    } catch (e) {
+      debugPrint('Error during login: $e');
     }
     return false;
   }
 
   Future<void> logout() async {
-    // Simulate API call
-    await Future.delayed(Duration(seconds: 1));
-    _isLoggedIn = false;
+    await _storage.delete(key: 'token');
   }
 }
